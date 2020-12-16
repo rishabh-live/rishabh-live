c1396b8837ffef8474de5c6f4b515e6f920493b0
Rishabh <34997913+rishabh-live@users.noreply.github.com>
2020-12-17 01:23:43 +0530

    Create update-github-activity.py

---
+++ update-github-activity.py
@@ -0,0 +1,44 @@
+import urllib
+import json
+
+USERNAME = "rishabh-live"
+
+f = open("./README.md", "r")
+
+fileArray = f.read().split('\n')
+f.close()
+
+indexPos = fileArray.index("<!-- START:github_activity -->")
+endPos = fileArray.index("<!-- END:github_activity -->")
+# print(fileArray)
+del fileArray[indexPos+1:endPos]
+# print(fileArray)
+# print(indexPos)
+
+writeData = "<!-- START:github_activity --> \n"
+url = "https://api.github.com/users/rishabh-live/events"
+
+response = urllib.urlopen(url)
+
+data = json.loads(response.read())
+i = 1
+for x in data:
+    event = x["payload"]["commits"][0]["message"]
+    repoName = x["repo"]["name"]
+    url = x["repo"]["url"]
+
+    writeData = writeData + str(i) + ") <a href=\"" + \
+        url+"\">"+event+" ( "+repoName+" )</a>\n"
+    i = i + 1
+
+fileArray[indexPos] = writeData
+# print(fileArray)
+
+theData = ""
+for words in fileArray:
+    theData = theData + words + "\n"
+
+f = open("./README.md", "w")
+f.write(theData)
+f.close()
+print(theData)
